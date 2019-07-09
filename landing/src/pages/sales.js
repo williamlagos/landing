2261dd4d8b589966883da36187fc1ab43b472b6d
William Oliveira de Lagos <william.lagos@icloud.com>
2019-07-09 16:22:19 -0300

    Added new optional grid framework and more items to sales landing page

--- landing/src/pages/sales.js
+++ landing/src/pages/sales.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import { Link } from 'gatsby'
-import { Box, Image, Stack/*, Button, Heading, Layer, Form, FormField*/ } from 'grommet'
+import { Box, Image, Stack, Text, Heading,/*, Button, Layer, Form, FormField*/ } from 'grommet'
 
 import Footer from "../components/Footer";
 import Layout from '../components/layout';
@@ -84,6 +84,162 @@ class SalesPage extends React.Component {
             </Container>
             <Image margin={{ "vertical": "medium" }} src={full} fill={true}/>
             <Container>
+              <Box id="shadow" align="center" pad="large" margin={{ "vertical": "medium" }}>
+                <Box direction="row" gap="medium">
+                  <div style={{ textAlign: "center" }}>
+                    <Image src={lock}/>
+                    <h5>Acesso Imediato</h5>
+                  </div>
+                  <div style={{ textAlign: "center" }}>
+                    <Image src={bulb}/>
+                    <h5>Mentoria Exclusiva</h5>
+                  </div>
+                  <div style={{ textAlign: "center" }}>
+                    <Image src={shield}/>
+                    <h5>Investimento 100% seguro</h5>
+                  </div>
+                </Box>
+                <br/>
+                <Link id="bigbutton" className="btn" to="videos/" >
+                  &nbsp; Faça parte da MoHub
+                </Link>
+              </Box>
+              <Box margin={{ "vertical": "medium" }}>
+                <Heading level={2} className="uppercase">O que você ganha ao entrar para a MoHub?</Heading>
+                <div className="row">
+                  <Stack className="six columns blue" anchor="top-left">
+                    <Heading className="title" level={2}>O Seu Ponto de Partida</Heading>
+                    <Box pad={{ "vertical": "36px" }} className="blue-gradient">
+                      <Heading textAlign="center" level={3}>Box 1</Heading>
+                    </Box>
+                    <Box className="text">
+                      <Text size="small">
+                        Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.
+                        Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.
+                        Vivamus suscipit tortor eget felis porttitor volutpat. Lorem
+                        ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+                        arcu erat, accumsan id imperdiet et, porttitor at sem. Nulla
+                        quis lorem ut libero malesuada feugiat.
+                      </Text>
+                    </Box>
+                    <Box margin={{"left": "220px", "top": "270px"}} anchor="center">
+                      <Heading className="risked" level={1}>
+                        R$ 879,00
+                      </Heading>
+                    </Box>
+                  </Stack>
+                  <Stack className="six columns blue" anchor="top-left">
+                    <Heading className="title" level={2}>O seu destino</Heading>
+                    <Box pad={{ "vertical": "36px" }} className="blue-gradient">
+                      <Heading textAlign="center" level={3}>Box 2</Heading>
+                    </Box>
+                    <Box className="text">
+                      <Text size="small">
+                        Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.
+                        Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.
+                        Vivamus suscipit tortor eget felis porttitor volutpat. Lorem
+                        ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+                        arcu erat, accumsan id imperdiet et, porttitor at sem. Nulla
+                        quis lorem ut libero malesuada feugiat.
+                      </Text>
+                    </Box>
+                    <Box margin={{"left": "220px", "top": "270px"}} anchor="center">
+                      <Heading className="risked" level={1}>
+                        R$ 879,00
+                      </Heading>
+                    </Box>
+                  </Stack>
+                </div>
+                <div className="row" style={{ "margin-top": "40px" }}>
+                  <Stack className="twelve columns blue" anchor="top-left">
+                    <Box pad={{ "vertical": "36px" }} className="blue-gradient">
+                      <Heading textAlign="center" level={3}>Box 3</Heading>
+                    </Box>
+                    <Box className="text" width="70%">
+                      <Heading level={2}>De Onde Vem o Dinheiro</Heading>
+                      <Text size="small">
+                        Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.
+                        Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.
+                        Vivamus suscipit tortor eget felis porttitor volutpat. Lorem
+                        ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+                        arcu erat, accumsan id imperdiet et, porttitor at sem. Nulla
+                        quis lorem ut libero malesuada feugiat.
+                      </Text>
+                    </Box>
+                    <Box margin={{"left": "720px", "top": "210px"}} anchor="center">
+                      <Heading className="risked" level={1}>
+                        R$ 879,00
+                      </Heading>
+                    </Box>
+                  </Stack>
+                </div>
+                <div className="row" style={{ "margin": "20px 0px" }}>
+                  <Stack className="four columns blue-high" anchor="top-left">
+                    <Box pad={{ "vertical": "36px" }} className="blue-gradient">
+                      <Heading textAlign="center" level={3}>Box 4</Heading>
+                    </Box>
+                    <Box className="title-under">
+                      <Heading level={2}>Momentum Bot</Heading>
+                      <Text size="small">
+                        Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.
+                        Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.
+                        Vivamus suscipit tortor eget felis porttitor volutpat. Lorem
+                        ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+                        arcu erat, accumsan id imperdiet et, porttitor at sem. Nulla
+                        quis lorem ut libero malesuada feugiat.
+                      </Text>
+                    </Box>
+                    <Box margin={{"left": "20px", "top": "430px"}} anchor="center">
+                      <Heading className="risked" level={2}>
+                        R$ 879,00
+                      </Heading>
+                    </Box>
+                  </Stack>
+                  <Stack className="four columns blue-high" anchor="top-left">
+                    <Box pad={{ "vertical": "36px" }} className="blue-gradient">
+                      <Heading textAlign="center" level={3}>Box 5</Heading>
+                    </Box>
+                    <Box className="title-under">
+                      <Heading level={2}>Escola de Negócios</Heading>
+                      <Text size="small">
+                        Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.
+                        Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.
+                        Vivamus suscipit tortor eget felis porttitor volutpat. Lorem
+                        ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+                        arcu erat, accumsan id imperdiet et, porttitor at sem. Nulla
+                        quis lorem ut libero malesuada feugiat.
+                      </Text>
+                    </Box>
+                    <Box margin={{"left": "20px", "top": "430px"}} anchor="center">
+                      <Heading className="risked" level={2}>
+                        R$ 879,00
+                      </Heading>
+                    </Box>
+                  </Stack>
+                  <Stack className="four columns blue-high" anchor="top-left">
+                    <Box pad={{ "vertical": "36px" }} className="blue-gradient">
+                      <Heading textAlign="center" level={3}>Box 6</Heading>
+                    </Box>
+                    <Box className="title-under">
+                      <Heading level={2}>O Seu Próprio Negócio</Heading>
+                      <Text size="small">
+                        Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.
+                        Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.
+                        Vivamus suscipit tortor eget felis porttitor volutpat. Lorem
+                        ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+                        arcu erat, accumsan id imperdiet et, porttitor at sem. Nulla
+                        quis lorem ut libero malesuada feugiat.
+                      </Text>
+                    </Box>
+                    <Box margin={{"left": "20px", "top": "430px"}} anchor="center">
+                      <Heading className="risked" level={2}>
+                        R$ 879,00
+                      </Heading>
+                    </Box>
+                  </Stack>
+                </div>
+              </Box>
+              <Box><Heading>PERGUNTAS FREQUENTES</Heading></Box>
               <Box id="shadow" align="center" pad="large" margin={{ "vertical": "medium" }}>
                 <Box direction="row" gap="medium">
                   <div style={{ textAlign: "center" }}>
