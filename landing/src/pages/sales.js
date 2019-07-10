6300cc6cb33f8bb854221a24e6c1aca7fc0a3644
William Oliveira de Lagos <william.lagos@icloud.com>
2019-07-10 05:43:17 -0300

    Added the last items to sales landing page

--- landing/src/pages/sales.js
+++ landing/src/pages/sales.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import { Link } from 'gatsby'
-import { Box, Image, Stack, Text, Heading,/*, Button, Layer, Form, FormField*/ } from 'grommet'
+import { Box, Image, Stack, Text, Heading, Collapsible, Button /*Layer, Form, FormField*/ } from 'grommet'
 
 import Footer from "../components/Footer";
 import Layout from '../components/layout';
@@ -14,6 +14,12 @@ import shield from "../assets/img/shield.png"
 import bulb from "../assets/img/bulb.png"
 import lock from "../assets/img/lock.png"
 import full from "../assets/img/full.png"
+import gift from "../assets/img/gift.png"
+import bag from "../assets/img/bag.png"
+import champ from "../assets/img/champ.png"
+
+import minus from "../assets/img/minus.png"
+import plus from "../assets/img/plus.png"
 
 /*const parallaxBackground = {
   backgroundImage: `url(${Background})`,
@@ -25,7 +31,13 @@ class SalesPage extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      toggledModal: false
+      toggledModal: false,
+      collapsed1: false,
+      collapsed2: false,
+      collapsed3: false,
+      collapsed4: false,
+      collapsed5: false,
+      collapsed6: false
     }
   }
 
@@ -36,6 +48,30 @@ class SalesPage extends React.Component {
 
   dismissModal() {
     this.setState({ toggledModal: false })
+  }
+
+  switchCollapsible1() {
+    this.setState({ collapsed1: this.state.collapsed1 ? false : true })
+  }
+
+  switchCollapsible2() {
+    this.setState({ collapsed2: this.state.collapsed2 ? false : true })
+  }
+
+  switchCollapsible3() {
+    this.setState({ collapsed3: this.state.collapsed3 ? false : true })
+  }
+
+  switchCollapsible4() {
+    this.setState({ collapsed4: this.state.collapsed4 ? false : true })
+  }
+
+  switchCollapsible5() {
+    this.setState({ collapsed5: this.state.collapsed5 ? false : true })
+  }
+
+  switchCollapsible6() {
+    this.setState({ collapsed6: this.state.collapsed6 ? false : true })
   }
 
   render() {
@@ -239,7 +275,163 @@ class SalesPage extends React.Component {
                   </Stack>
                 </div>
               </Box>
-              <Box><Heading>PERGUNTAS FREQUENTES</Heading></Box>
+              <Box pad="medium" direction="row" background="#42434e" fill="horizontal">
+                <Box className="right-gradient" alignSelf="center" pad="medium" basis="30%">
+                  <Image margin="medium" fit="contain" src={gift}/>
+                  <Heading textAlign="center">2 BÔNUS</Heading>
+                  <Heading textAlign="center" className="risked" level={2}>
+                    R$ 879,00
+                  </Heading>
+                </Box>
+                <Box alignSelf="center" basis="70%">
+                  <Box alignSelf="center" direction="row">
+                    <Box alignSelf="center" pad="medium" basis="30%">
+                      <Image margin="small" fit="contain" src={champ}/>
+                      <Heading textAlign="center" level={2}>Bônus 1</Heading>
+                    </Box>
+                    <Box pad="medium" basis="70%">
+                      <Heading level={2}>Como Sair das Dívidas</Heading>
+                      <Text size="small">
+                        Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.
+                        Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.
+                        Vivamus suscipit tortor eget felis porttitor volutpat. Lorem
+                        ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+                        arcu erat, accumsan id imperdiet et, porttitor at sem. Nulla
+                        quis lorem ut libero malesuada feugiat.
+                      </Text>
+                    </Box>
+                  </Box>
+                  <Box alignSelf="center" direction="row">
+                    <Box alignSelf="center" pad="medium" basis="30%">
+                      <Image margin="small" fit="contain" src={bag}/>
+                      <Heading textAlign="center" level={2}>Bônus 2</Heading>
+                    </Box>
+                    <Box pad="medium" basis="70%">
+                      <Heading level={2}>Dicas de Renda Extra</Heading>
+                      <Text size="small">
+                        Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.
+                        Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.
+                        Vivamus suscipit tortor eget felis porttitor volutpat. Lorem
+                        ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+                        arcu erat, accumsan id imperdiet et, porttitor at sem. Nulla
+                        quis lorem ut libero malesuada feugiat.
+                      </Text>
+                    </Box>
+                  </Box>
+                </Box>
+              </Box>
+              <Box margin="medium"></Box>
+              <Box>
+                <Heading>PERGUNTAS FREQUENTES</Heading>
+                <Stack className="stack" anchor="right">
+                  <Button fill={true} className="gray-button" plain={true} onClick={(e) => this.switchCollapsible1()} label='Será que isso funciona pra mim?'/>
+                  <Image className="gray-symbol" pad="medium" size="contain" src={this.state.collapsed1 ? minus : plus}/>
+                </Stack>
+                <Collapsible direction="vertical" open={this.state.collapsed1}>
+                  <Box pad="medium" background="#e1e1e7">
+                    <Text>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut fermentum auctor magna eget volutpat.
+                    Nam sagittis ex eget mollis vestibulum. Donec vestibulum ullamcorper nisi, ac lacinia arcu dictum
+                    vitae. Nunc varius sed elit sit amet laoreet. Integer convallis sagittis semper. Proin libero leo,
+                    vestibulum ac consectetur vel, fermentum consectetur magna. Nunc lacinia, lorem quis mattis placerat,
+                    velit tellus luctus turpis, non gravida quam metus quis enim. Cras et ipsum libero. Praesent libero
+                    libero, pretium vehicula orci non, feugiat elementum justo. Maecenas elit leo, vulputate id tortor
+                    quis, convallis molestie tellus. Phasellus nec mi sit amet libero hendrerit bibendum nec id urna.
+                    Praesent vel malesuada eros. Nulla eleifend purus id risus mollis congue eget et ex.
+                    </Text>
+                  </Box>
+                </Collapsible>
+                <Stack className="stack" anchor="right">
+                  <Button fill={true} className="gray-button" plain={true} onClick={(e) => this.switchCollapsible2()} label='Como terei acesso aos Boxes?'/>
+                  <Image className="gray-symbol" pad="medium" size="contain" src={this.state.collapsed2 ? minus : plus}/>
+                </Stack>
+                <Collapsible direction="vertical" open={this.state.collapsed2}>
+                  <Box pad="medium" background="#e1e1e7">
+                    <Text>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut fermentum auctor magna eget volutpat.
+                    Nam sagittis ex eget mollis vestibulum. Donec vestibulum ullamcorper nisi, ac lacinia arcu dictum
+                    vitae. Nunc varius sed elit sit amet laoreet. Integer convallis sagittis semper. Proin libero leo,
+                    vestibulum ac consectetur vel, fermentum consectetur magna. Nunc lacinia, lorem quis mattis placerat,
+                    velit tellus luctus turpis, non gravida quam metus quis enim. Cras et ipsum libero. Praesent libero
+                    libero, pretium vehicula orci non, feugiat elementum justo. Maecenas elit leo, vulputate id tortor
+                    quis, convallis molestie tellus. Phasellus nec mi sit amet libero hendrerit bibendum nec id urna.
+                    Praesent vel malesuada eros. Nulla eleifend purus id risus mollis congue eget et ex.
+                    </Text>
+                  </Box>
+                </Collapsible>
+                <Stack className="stack" anchor="right">
+                  <Button fill={true} className="gray-button" plain={true} onClick={(e) => this.switchCollapsible3()} label='Há algum custo de renovação ou pagamento mensal?'/>
+                  <Image className="gray-symbol" pad="medium" size="contain" src={this.state.collapsed3 ? minus : plus}/>
+                </Stack>
+                <Collapsible direction="vertical" open={this.state.collapsed3}>
+                  <Box pad="medium" background="#e1e1e7">
+                    <Text>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut fermentum auctor magna eget volutpat.
+                    Nam sagittis ex eget mollis vestibulum. Donec vestibulum ullamcorper nisi, ac lacinia arcu dictum
+                    vitae. Nunc varius sed elit sit amet laoreet. Integer convallis sagittis semper. Proin libero leo,
+                    vestibulum ac consectetur vel, fermentum consectetur magna. Nunc lacinia, lorem quis mattis placerat,
+                    velit tellus luctus turpis, non gravida quam metus quis enim. Cras et ipsum libero. Praesent libero
+                    libero, pretium vehicula orci non, feugiat elementum justo. Maecenas elit leo, vulputate id tortor
+                    quis, convallis molestie tellus. Phasellus nec mi sit amet libero hendrerit bibendum nec id urna.
+                    Praesent vel malesuada eros. Nulla eleifend purus id risus mollis congue eget et ex.
+                    </Text>
+                  </Box>
+                </Collapsible>
+                <Stack className="stack" anchor="right">
+                  <Button fill={true} className="gray-button" plain={true} onClick={(e) => this.switchCollapsible4()} label='O conteúdo da Mohub é todo em vídeo?'/>
+                  <Image className="gray-symbol" pad="medium" size="contain" src={this.state.collapsed4 ? minus : plus}/>
+                </Stack>
+                <Collapsible direction="vertical" open={this.state.collapsed4}>
+                  <Box pad="medium" background="#e1e1e7">
+                    <Text>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut fermentum auctor magna eget volutpat.
+                    Nam sagittis ex eget mollis vestibulum. Donec vestibulum ullamcorper nisi, ac lacinia arcu dictum
+                    vitae. Nunc varius sed elit sit amet laoreet. Integer convallis sagittis semper. Proin libero leo,
+                    vestibulum ac consectetur vel, fermentum consectetur magna. Nunc lacinia, lorem quis mattis placerat,
+                    velit tellus luctus turpis, non gravida quam metus quis enim. Cras et ipsum libero. Praesent libero
+                    libero, pretium vehicula orci non, feugiat elementum justo. Maecenas elit leo, vulputate id tortor
+                    quis, convallis molestie tellus. Phasellus nec mi sit amet libero hendrerit bibendum nec id urna.
+                    Praesent vel malesuada eros. Nulla eleifend purus id risus mollis congue eget et ex.
+                    </Text>
+                  </Box>
+                </Collapsible>
+                <Stack className="stack" anchor="right">
+                  <Button fill={true} className="gray-button" plain={true} onClick={(e) => this.switchCollapsible5()} label='Quando os boxes serão liberados?'/>
+                  <Image className="gray-symbol" pad="medium" size="contain" src={this.state.collapsed5 ? minus : plus}/>
+                </Stack>
+                <Collapsible direction="vertical" open={this.state.collapsed5}>
+                  <Box pad="medium" background="#e1e1e7">
+                    <Text>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut fermentum auctor magna eget volutpat.
+                    Nam sagittis ex eget mollis vestibulum. Donec vestibulum ullamcorper nisi, ac lacinia arcu dictum
+                    vitae. Nunc varius sed elit sit amet laoreet. Integer convallis sagittis semper. Proin libero leo,
+                    vestibulum ac consectetur vel, fermentum consectetur magna. Nunc lacinia, lorem quis mattis placerat,
+                    velit tellus luctus turpis, non gravida quam metus quis enim. Cras et ipsum libero. Praesent libero
+                    libero, pretium vehicula orci non, feugiat elementum justo. Maecenas elit leo, vulputate id tortor
+                    quis, convallis molestie tellus. Phasellus nec mi sit amet libero hendrerit bibendum nec id urna.
+                    Praesent vel malesuada eros. Nulla eleifend purus id risus mollis congue eget et ex.
+                    </Text>
+                  </Box>
+                </Collapsible>
+                <Stack className="stack" anchor="right">
+                  <Button fill={true} className="gray-button" plain={true} onClick={(e) => this.switchCollapsible6()} label='Por que Mohub?'/>
+                  <Image className="gray-symbol" pad="medium" size="contain" src={this.state.collapsed6 ? minus : plus}/>
+                </Stack>
+                <Collapsible direction="vertical" open={this.state.collapsed6}>
+                  <Box pad="medium" background="#e1e1e7">
+                    <Text>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut fermentum auctor magna eget volutpat.
+                    Nam sagittis ex eget mollis vestibulum. Donec vestibulum ullamcorper nisi, ac lacinia arcu dictum
+                    vitae. Nunc varius sed elit sit amet laoreet. Integer convallis sagittis semper. Proin libero leo,
+                    vestibulum ac consectetur vel, fermentum consectetur magna. Nunc lacinia, lorem quis mattis placerat,
+                    velit tellus luctus turpis, non gravida quam metus quis enim. Cras et ipsum libero. Praesent libero
+                    libero, pretium vehicula orci non, feugiat elementum justo. Maecenas elit leo, vulputate id tortor
+                    quis, convallis molestie tellus. Phasellus nec mi sit amet libero hendrerit bibendum nec id urna.
+                    Praesent vel malesuada eros. Nulla eleifend purus id risus mollis congue eget et ex.
+                    </Text>
+                  </Box>
+                </Collapsible>
+              </Box>
               <Box id="shadow" align="center" pad="large" margin={{ "vertical": "medium" }}>
                 <Box direction="row" gap="medium">
                   <div style={{ textAlign: "center" }}>
